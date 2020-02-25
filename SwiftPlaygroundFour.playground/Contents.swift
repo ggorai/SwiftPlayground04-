24ab54090461aa20b98823f9bc139c41a064b837
CM Student <cmstudent@UHWO-ACM-C-14.local>
2020-02-25 10:20:32 -1000

    Completed Parts 1-6

---
+++ SwiftPlaygroundFour.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Part04
+for number in 1...5{
+    print(number)
+}
+
+//Part05
+var sum = 0
+for number in 1...5{
+    sum += number
+    print(sum)
+}
+
+
+
+//Part06 (While Loops)
+
+var i = 1
+while i <= 10
+{
+    
+    i = i + 1
+        print(i)
+}
